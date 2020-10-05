ab4e0be57e4afd1abc8cb1ff4dd88015a462a93e
Alaa-shaban <ashabaan@cloudinn.net>
2020-10-05 19:58:02 +0200

    generate elements.js and include to to our index file

---
+++ projects/elements/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { ComponentsModule, ComponentsComponent } from 'components';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ComponentsModule
+  ],
+  providers: []
+})
+export class AppModule {
+
+  constructor(private injector: Injector) { }
+
+  // tslint:disable-next-line: typedef
+  ngDoBootstrap() {
+    const element = createCustomElement(ComponentsComponent, { injector: this.injector });
+    customElements.define('lib-components', element);
+  }
+
+}
